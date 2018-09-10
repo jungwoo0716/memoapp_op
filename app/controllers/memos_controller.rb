079ceb512b950355f8a27ed8e706ed66c9927346
jungwoo <kimjungwoo0716@gmail.com>
2018-09-10 11:45:28 +0000

    memoapp(3)

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,11 +1,24 @@
 class MemosController < ApplicationController
   def index
+    @memos = Memo.all
   end
 
   def new
   end
 
   def create
+    @memo = Memo.new
+    @memo.title = params[:memo][:title]
+    @memo.content = params[:memo][:content]
+    @memo.user_id = params[:memo][:user_id]
+    @memo.save
+    
+    # redirect_to memo_path(@memo)
+    redirect_to @memo
+  end
+
+  def show
+    @memo = Memo.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   
-  root 'home#index'
+  root 'memos#index'
   
   devise_for :users
   resources :memos
-  get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
